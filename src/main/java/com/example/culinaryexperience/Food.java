34b8cb575812e6aed535c80b52b5ec9931abb1a3
noyTony <noycohen982@gmail.com>
2022-02-17 20:16:31 +0200

    add food class

---
+++ src/main/java/com/example/culinaryexperience/Food.java
@@ -0,0 +1,12 @@
+package com.example.culinaryexperience;
+
+import java.util.List;
+
+public class Food {
+    private String name;
+    private String recipe;
+    private String category;
+    private String category2;
+    private List<String> alcohol;
+    private String imageName;
+}
